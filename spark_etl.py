8a999efa62fab1b8def141b9263250b6074c1997
hieppham <hieppham>
2020-04-11 00:45:50 +0200

    finish ETL script

---
+++ spark_etl.py
@@ -0,0 +1,221 @@
+import json
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, monotonically_increasing_id
+import pandas as pd
+
+
+LOAN_INPUT_PATH = 's3n://lending-club-loan//raw_data/201812.csv'
+OUTPUT_PATH = 's3n://lending-club-loan//transformed_data/'
+DEMO_DATA_PATH = 'https://public.opendatasoft.com/explore/dataset/us-cities-demographics/download/?format=json&timezone=Europe/Berlin&lang=en'
+
+
+def main():
+    
+    # process loan data
+    df_loan = spark.read.csv(LOAN_INPUT_PATH, header=True)
+    
+    process_lending_club_data(df_loan, OUTPUT_PATH)
+    
+    # process state demographic data
+    df_demo = get_state_demographic(DEMO_DATA_PATH)
+    
+    df_demo.to_csv(OUTPUT_PATH + '//demographic//state_demo.csv')
+    
+    return None
+
+
+def process_lending_club_data(df, output_path):
+        
+    df = general_data_processing(df)
+    
+    process_loan_data(df, output_path)
+    
+    process_borrower_data(df, output_path)
+    
+    process_payment_data(df, output_path)
+    
+    process_credit_hist_data(df, output_path)
+    
+    process_bad_debt_settlement(df, output_path)
+    
+    process_hardship_data(df, output_path)
+    
+    
+    return None
+    
+
+def general_data_processing(df):
+    
+    df = df.withColumn('loan_id', monotonically_increasing_id())
+    df = df.withColumn('borrower_id', monotonically_increasing_id())
+    df = _parse_dates(df)
+    df = _convert_term_to_num(df)
+    
+    return df
+
+def _parse_dates(df):
+    
+    date_cols = ['issue_d', 'payment_plan_start_date', 'hardship_start_date', 'hardship_end_date', 'next_pymnt_d', 'last_pymnt_d', 
+                 'earliest_cr_line', 'last_credit_pull_d', 'sec_app_earliest_cr_line', 'debt_settlement_flag_date', 'settlement_date']
+
+    month_letter_to_num = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
+                               'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+    
+    parse_date = udf(lambda x: x[-4:] + '-' + month_letter_to_num[x[:3]] + '-' + '15' if  x != None else None)
+    
+    for col in date_cols:
+        
+        df = df.withColumn(col, parse_date(df[col]))
+        
+    return df
+
+def _convert_term_to_num(df):
+    
+    convert_term = udf(lambda x: int(x[:-7]))
+    
+    df = df.withColumn('term', convert_term(df.term))
+    
+    return df
+
+
+
+def process_loan_data(df, output_path):
+    
+    loan_cols = ['loan_id', 'borrower_id','issue_d', 'term', 'loan_amnt', 'funded_amnt', 'funded_amnt_inv','int_rate', 'installment' ,
+                 'grade', 'sub_grade', 'initial_list_status', 'application_type', 'verification_status', 
+                 'verification_status_joint', 'pymnt_plan', 'url', 'desc', 'purpose', 'title',  'dti', 'dti_joint']
+    
+    df.select(loan_cols)\
+      .write\
+      .parquet(os.path.join(output_path, 'loans'),  'overwrite')
+    
+    return None
+
+
+def process_borrower_data(df, output_path):
+    
+    borrower_cols = ['borrower_id', 'annual_inc', 'annual_inc_joint',  'emp_length', 'emp_title', 
+                     'home_ownership',  'zip_code', 'addr_state']
+    
+    df.select(borrower_cols)\
+      .write\
+      .parquet(os.path.join(output_path, 'borrowers'),  'overwrite')
+                     
+    return None
+
+
+def process_credit_hist_data(df, output_path):
+    
+    credit_hist_cols = ['borrower_id', 'earliest_cr_line',  'last_credit_pull_d', 
+                         'acc_now_delinq', 'acc_open_past_24mths', 'all_util', 'avg_cur_bal', 'bc_open_to_buy',
+                         'bc_util', 'chargeoff_within_12_mths', 'collections_12_mths_ex_med', 'delinq_2yrs',
+                         'delinq_amnt', 'il_util', 'inq_fi', 'inq_last_12m', 'inq_last_6mths',
+                         'max_bal_bc', 'mo_sin_old_il_acct', 'mo_sin_old_rev_tl_op', 'mo_sin_rcnt_rev_tl_op',
+                         'mo_sin_rcnt_tl', 'mort_acc', 'mths_since_last_delinq', 'mths_since_last_major_derog',
+                         'mths_since_last_record', 'mths_since_rcnt_il', 'mths_since_recent_bc', 
+                         'mths_since_recent_bc_dlq','mths_since_recent_inq', 'mths_since_recent_revol_delinq', 
+                         'num_accts_ever_120_pd', 'num_actv_bc_tl', 'num_actv_rev_tl', 'num_bc_sats', 'num_bc_tl', 
+                         'num_il_tl', 'num_op_rev_tl', 'num_rev_accts', 'num_rev_tl_bal_gt_0', 'num_sats', 
+                         'num_tl_120dpd_2m', 'num_tl_30dpd', 'num_tl_90g_dpd_24m', 'num_tl_op_past_12m', 'open_acc', 
+                         'open_acc_6m', 'open_il_12m', 'open_il_24m', 'open_act_il', 'open_rv_12m', 'open_rv_24m', 
+                         'pct_tl_nvr_dlq', 'percent_bc_gt_75', 'policy_code', 'pub_rec', 'pub_rec_bankruptcies', 
+                         'revol_bal', 'revol_util', 'tax_liens', 'tot_coll_amt', 'tot_cur_bal', 'tot_hi_cred_lim', 
+                         'total_acc', 'total_bal_ex_mort', 'total_bal_il', 'total_bc_limit', 'total_cu_tl', 
+                         'total_il_high_credit_limit', 'sec_app_open_act_il', 'total_rev_hi_lim', 'revol_bal_joint',
+                         'sec_app_earliest_cr_line', 'sec_app_inq_last_6mths', 'sec_app_mort_acc', 'sec_app_open_acc',
+                         'sec_app_revol_util', 'sec_app_num_rev_accts', 'sec_app_chargeoff_within_12_mths',
+                         'sec_app_collections_12_mths_ex_med', 'sec_app_mths_since_last_major_derog']
+    
+    df.select(credit_hist_cols)\
+      .write\
+      .parquet(os.path.join(output_path, 'credit_history'),  'overwrite')
+    
+    return None
+
+
+def process_payment_date(df, output_path):
+    
+    payment_cols = ['loan_id', 'loan_status', 'total_pymnt', 'last_pymnt_amnt', 'recoveries', 'collection_recovery_fee',
+                    'out_prncp', 'out_prncp_inv', 'total_pymnt_inv', 'total_rec_prncp', 'total_rec_int', 
+                    'total_rec_late_fee', 'next_pymnt_d', 'last_pymnt_d']
+    
+    df.select(payment_cols)\
+      .write\
+      .parquet(os.path.join(output_path, 'payment'),  'overwrite')
+    
+    return None
+
+
+def process_bad_debt_settlement(df, output_path):
+    
+    bad_debt_cols = ['loan_id', 'debt_settlement_flag', 'debt_settlement_flag_date', 'settlement_status',
+                 'settlement_date', 'settlement_amount', 'settlement_percentage', 'settlement_term']
+    
+    df = df.select(bad_debt_cols)\
+           .filter('debt_settlement_flag=="Y"')
+    
+    df.write\
+      .parquet(os.path.join(output_path, 'bad_debt_settlement'),  'overwrite')
+    
+    return None
+
+
+def process_hardship_data(df, output_path):
+    
+    hardship_cols = ['loan_id', 'hardship_flag', 'hardship_type', 'hardship_reason', 'hardship_status', 
+                     'deferral_term', 'hardship_amount', 'hardship_start_date', 'hardship_end_date',
+                     'payment_plan_start_date', 'hardship_length', 'hardship_dpd', 'hardship_loan_status',
+                     'orig_projected_additional_accrued_interest', 'hardship_payoff_balance_amount',
+                     'hardship_last_payment_amount']
+    
+    df = df.select(hardship_cols)\
+           .filter('hardship_flag == "Y"')
+    
+    df.write\
+      .parquet(os.path.join(output_path, 'hardship'),  'overwrite')
+    
+    return None
+    
+
+def get_state_demographic(json_url):
+    
+    df_demo = pd.read_json(json_url)
+    df_demo = pd.DataFrame(df_demo.fields.tolist())
+    
+    # extract population by race by city
+    df_race_city = df_demo[['city', 'race', 'count']]
+    df_race_city = pd.pivot_table(data=df_race_city, values='count', index='city', columns='race').reset_index()
+    df_race_city.columns = ['city', 'pop_american_natives', 'pop_asian', 'pop_black', 'pop_hispanic','pop_white']
+
+    # get unique records for each city 
+    df_demo_city = df_demo[['city', 'state', 'state_code', 'median_age', 'male_population', 'female_population', 
+                            'count', 'number_of_veterans', 'foreign_born', 'average_household_size']]
+    df_demo_city = df_demo_city.drop_duplicates()
+    df_demo_city.columns = ['city', 'state_name', 'state_code', 'median_age', 'pop_male', 'pop_female', 'pop_total', 'no_vetagans', 'foreign_born', 'avg_house_size']
+
+    # merge city demographic data with population by city and race
+    df_demo_city = df_demo_city.merge(df_race_city, how='left', on='city')
+
+    # aggregate to State level
+    df_demo_state = df_demo_city.groupby(['state_code', 'state_name']).agg({'median_age': 'mean',
+                                                                       'avg_house_size': 'mean',
+                                                                       'pop_total': 'sum',
+                                                                       'foreign_born': 'sum',
+                                                                       'no_vetagans': 'sum',
+                                                                       'pop_male': 'sum',
+                                                                       'pop_female': 'sum',
+                                                                       'pop_american_natives': 'sum',
+                                                                       'pop_asian': 'sum',
+                                                                       'pop_black': 'sum',
+                                                                       'pop_hispanic': 'sum',
+                                                                       'pop_white': 'sum'
+                                                                      })
+    df_demo_state = df_demo_state.reset_index()
+    
+    return df_demo_state
+
+
+if __name__ == "__main__":
+    main()
---
+++ sql_queries.py
@@ -0,0 +1,292 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+LOANS_BUCKET = config['S3']['LOANS']
+BORROWERS_BUCKET = config['S3']['BORROWERS']
+PAYMENT_BUCKET = config['S3']['PAYMENT']
+CREDIT_HIST_BUCKET= config['S3']['CREDIT_HIST']
+BAD_DEBT_BUCKET = config['S3']['BAD_DEBT']
+HARDSHIP_BUCKET = config['S3']['HARDSHIP']
+STATE_DEMO_BUCKET = config['S3']['STATE_DEMO']
+ARN = config.get("IAM_ROLE","ARN")
+
+# DROP TABLES
+
+loans_table_drop = "DROP TABLE IF EXISTS loans"
+borrowers_table_drop = "DROP TABLE IF EXISTS borrowers"
+payment_table_drop = "DROP TABLE IF EXISTS payment"
+credit_history_table_drop = "DROP TABLE IF EXISTS credit_history"
+bad_debt_settlement_table_drop = "DROP TABLE IF EXISTS bad_debt_settlement"
+hardship_table_drop = "DROP TABLE IF EXISTS hardship"
+state_demo_table_drop = "DROP TABLE IF EXISTS state_demo"
+
+# CREATE TABLES
+loans_table_create= ("""
+    CREATE TABLE IF NOT EXISTS loans (
+                loan_id                     INT NOT NULL,
+                borrower_id                 INT,
+                issue_d                     TIMESTAMP,
+                term                        INT,
+                loan_amnt                   FLOAT,
+                funded_amnt                 FLOAT,
+                funded_amnt_inv             FLOAT,
+                int_rate                    FLOAT,
+                installment                 FLOAT,
+                grade                       VARCHAR,
+                sub_grade                   VARCHAR,
+                initial_list_status         VARCHAR,
+                application_type            VARCHAR,
+                verification_status         VARCHAR,
+                verification_status_joint   VARCHAR,
+                pymnt_plan                  VARCHAR,
+                url                         VARCHAR,
+                descrb                      VARCHAR,
+                purpose                     VARCHAR,
+                title                       VARCHAR,
+                dti                         FLOAT,
+                dti_joint                   FLOAT
+    );
+""")
+
+borrowers_table_create = ("""
+CREATE TABLE IF NOT EXISTS borrowers (
+                borrower_id         INT NOT NULL,
+                annual_inc          FLOAT,
+                annual_inc_joint    FLOAT,
+                emp_length          VARCHAR,
+                emp_title           VARCHAR,
+                home_ownership      VARCHAR,
+                zip_code            VARCHAR,
+                addr_state          VARCHAR
+    );
+""")
+
+
+payment_table_create = ("""
+CREATE TABLE IF NOT EXISTS payment (
+                loan_id                     INT,
+                loan_status                 VARCHAR,
+                total_pymnt                 FLOAT,
+                last_pymnt_amnt             FLOAT,
+                recoveries                  FLOAT,
+                collection_recovery_fee     FLOAT,
+                out_prncp                   FLOAT,
+                out_prncp_inv               FLOAT,
+                total_pymnt_inv             FLOAT,
+                total_rec_prncp             FLOAT,
+                total_rec_int               FLOAT,
+                total_rec_late_fee          FLOAT,
+                next_pymnt_d                TIMESTAMP,
+                last_pymnt_d                TIMESTAMP
+);
+""")
+
+
+credit_history_table_create = ("""
+CREATE TABLE IF NOT EXISTS credit_history (
+                borrower_id                             INT NOT NULL,
+                earliest_cr_line                        TIMESTAMP,
+                last_credit_pull_d                      TIMESTAMP,
+                acc_now_delinq                          INT,
+                acc_open_past_24mths                    INT,
+                all_util                                FLOAT,
+                avg_cur_bal                             FLOAT,
+                bc_open_to_buy                          FLOAT,
+                bc_util                                 FLOAT,
+                chargeoff_within_12_mths                FLOAT,
+                collections_12_mths_ex_med              FLOAT,
+                delinq_2yrs                             FLOAT,
+                delinq_amnt                             FLOAT,
+                il_util                                 FLOAT,
+                inq_fi                                  INT,
+                inq_last_12m                            INT,
+                inq_last_6mths                          INT,
+                max_bal_bc                              FLOAT,
+                mo_sin_old_il_acct                      INT,
+                mo_sin_old_rev_tl_op                    INT,
+                mo_sin_rcnt_rev_tl_op                   INT,
+                mo_sin_rcnt_tl                          INT, 
+                mort_acc                                INT, 
+                mths_since_last_delinq                  INT, 
+                mths_since_last_major_derog             INT,
+                mths_since_last_record                  INT, 
+                mths_since_rcnt_il                      INT, 
+                mths_since_recent_bc                    INT,
+                mths_since_recent_bc_dlq                INT,
+                mths_since_recent_inq                   INT, 
+                mths_since_recent_revol_delinq          INT,
+                num_accts_ever_120_pd                   INT, 
+                num_actv_bc_tl                          INT, 
+                num_actv_rev_tl                         INT, 
+                num_bc_sats                             INT, 
+                num_bc_tl                               INT, 
+                num_il_tl                               INT, 
+                num_op_rev_tl                           INT, 
+                num_rev_accts                           INT, 
+                num_rev_tl_bal_gt_0                     INT, 
+                num_sats                                INT, 
+                num_tl_120dpd_2m                        INT, 
+                num_tl_30dpd                            INT, 
+                num_tl_90g_dpd_24m                      INT,
+                num_tl_op_past_12m                      INT, 
+                open_acc                                INT, 
+                open_acc_6m                             INT, 
+                open_il_12m                             INT, 
+                open_il_24m                             INT, 
+                open_act_il                             INT, 
+                open_rv_12m                             INT, 
+                open_rv_24m                             INT, 
+                pct_tl_nvr_dlq                          INT, 
+                percent_bc_gt_75                        FLOAT, 
+                policy_code                             INT, 
+                pub_rec                                 INT,   
+                pub_rec_bankruptcies                    INT,
+                revol_bal                               FLOAT, 
+                revol_util                              FLOAT, 
+                tax_liens                               FLOAT, 
+                tot_coll_amt                            FLOAT, 
+                tot_cur_bal                             FLOAT, 
+                tot_hi_cred_lim                         FLOAT,
+                total_acc                               FLOAT, 
+                total_bal_ex_mort                       FLOAT, 
+                total_bal_il                            FLOAT, 
+                total_bc_limit                          FLOAT, 
+                total_cu_tl                             FLOAT, 
+                total_il_high_credit_limit              FLOAT, 
+                sec_app_open_act_il                     INT, 
+                total_rev_hi_lim                        FLOAT, 
+                revol_bal_joint                         FLOAT, 
+                sec_app_earliest_cr_line                TIMESTAMP, 
+                sec_app_inq_last_6mths                  INT, 
+                sec_app_mort_acc                        FLOAT, 
+                sec_app_open_acc                        INT,
+                sec_app_revol_util                      FLOAT,
+                sec_app_num_rev_accts                   INT, 
+                sec_app_chargeoff_within_12_mths        FLOAT,
+                sec_app_collections_12_mths_ex_med      FLOAT, 
+                sec_app_mths_since_last_major_derog     FLOAT
+      );
+""")
+
+
+bad_debt_settlement_table_create = ("""
+CREATE TABLE IF NOT EXISTS bad_debt_settlement (
+                loan_id                     INT,
+                debt_settlement_flag_date   TIMESTAMP,
+                settlement_status           VARCHAR,
+                settlement_date             TIMESTAMP,
+                settlement_amount           FLOAT,
+                settlement_percentage       FLOAT,
+                settlement_term             INT
+  );
+""")
+
+hardship_table_create = ("""
+CREATE TABLE IF NOT EXISTS hardshift (
+               loan_id                                      INT,
+               hardship_type                                VARCHAR,
+               hardship_reason                              VARCHAR,
+               hardship_status                              VARCHAR,
+               deferral_term                                INT,
+               hardship_amount                              FLOAT,
+               hardship_start_date                          TIMESTAMP,
+               hardship_end_date                            TIMESTAMP,
+               payment_plan_start_date                      TIMESTAMP,
+               hardship_length                              INT,
+               hardship_dpd                                 INT,
+               hardship_loan_status                         VARCHAR,
+               orig_projected_additional_accrued_interest   FLOAT,
+               hardship_payoff_balance_amount               FLOAT,
+               hardship_last_payment_amount                 FLOAT
+  );
+""")
+
+state_demo_table_create = ("""
+CREATE TABLE IF NOT EXISTS state_demo (
+              state_code                VARCHAR NOT NULL,
+              state_name                VARCHAR,
+              median_age                FLOAT,
+              avg_house_size            FLOAT,
+              pop_total                 INT,
+              foreign_born              INT,
+              no_vetagans               INT,
+              pop_male                  INT,
+              pop_female                INT,
+              pop_american_natives      INT,
+              pop_asian                 INT,
+              pop_black                 INT,
+              pop_hispanic              INT,
+              pop_white                 INT
+   );
+""")
+
+# INSERT TABLES
+loans_table_insert = ("""
+    COPY loans 
+    FROM '{}'
+    IAM_ROLE '{}'
+    FORMAT AS PARQUET;
+""").format(LOANS_BUCKET, ARN)
+
+
+borrowers_table_insert = ("""
+    COPY borrowers 
+    FROM '{}'
+    iam_role '{}'
+    format as PARQUET;
+""").format(BORROWERS_BUCKET, ARN)
+
+
+payment_table_insert = ("""
+    COPY payment 
+    FROM '{}'
+    iam_role '{}'
+    format as PARQUET;
+""").format(PAYMENT_BUCKET, ARN)
+
+
+credit_history_table_insert = ("""
+    COPY credit_history 
+    FROM '{}'
+    iam_role '{}'
+    format as PARQUET;
+""").format(CREDIT_HIST_BUCKET, ARN)
+
+
+
+bad_debt_settlement_table_insert = ("""
+    COPY bad_debt_settlement 
+    FROM '{}'
+    iam_role '{}'
+    format as PARQUET;;
+""").format(BAD_DEBT_BUCKET, ARN)
+
+hardship_table_insert = ("""
+    COPY hardship 
+    FROM '{}'
+    iam_role '{}'
+    format as PARQUET;
+""").format(HARDSHIP_BUCKET, ARN)
+
+state_demo_table_insert = ("""
+    COPY state_demo 
+    FROM '{}'
+    iam_role '{}'
+    format as csv;
+""").format(STATE_DEMO_BUCKET, ARN)
+
+# QUERY LISTS
+
+create_table_queries = [loans_table_create, borrowers_table_create, payment_table_create, credit_history_table_create, 
+                        bad_debt_settlement_table_create, hardship_table_create, state_demo_table_create]
+
+drop_table_queries = [loans_table_drop, borrowers_table_drop, payment_table_drop, credit_history_table_drop, 
+                      bad_debt_settlement_table_drop, hardship_table_drop, state_demo_table_drop]
+
+#insert_table_queries = [loans_table_insert, borrowers_table_insert, payment_table_insert, credit_history_table_insert, 
+#                        bad_debt_settlement_table_insert, hardship_table_insert, state_demo_table_insert]
+
+insert_table_queries = [state_demo_table_insert]
